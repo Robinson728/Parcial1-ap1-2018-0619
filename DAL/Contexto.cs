5cf69307503b60e1427076ec0301c3e2267d1dd0
Robinson728 <77537741+Robinson728@users.noreply.github.com>
2021-02-08 19:19:52 -0400

    Conectando Entidad Ciudades con la base de datos

--- DAL/Contexto.cs
+++ DAL/Contexto.cs
@@ -1,10 +1,18 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using PrimerParcial.Entidades;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace PrimerParcial.DAL
 {
-    class Contexto
+    public class Contexto : DbContext
     {
+        public DbSet<Ciudades> Ciudades { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source = GestionCiudades.Db");
+        }
     }
 }
